a04b9d0a1cf8479f7ed183d411bb338fbd9af8da
Andres Root <info@andresroot.co>
2019-03-14 16:09:23 -0700

    Add new styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,51 @@
-console.log('\'Allo \'Allo!');
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+const SELECTORS = {
+  'tvButton': '.tv__button',
+  'channel': '.tv__channel',
+  'active': '.active',
+};
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+const CLASSNAMES = {
+  'active': 'active',
+};
+
+class Television {
+  constructor(element) {
+    this.element = element;
+    this.buttons = this.element.querySelectorAll(SELECTORS.tvButton);
+    this.channels = this.element.querySelectorAll(SELECTORS.channel);
+    this.currentChannel = 0;
+
+    this.init();
+  }
+
+  init() {
+    // Add event handlers
+    this.buttons.forEach(element => {
+      element.addEventListener('click', () => {
+        this.setChannel(element.dataset.channel);
+      });
+    });
+  }
+
+  setChannel(channel) {
+    // Hide current active channel
+    this.channels[this.currentChannel].classList.remove(CLASSNAMES.active);
+
+    // Enable requested channel
+    this.channels[channel].classList.add(CLASSNAMES.active);
+
+    // Set new current channel in memory
+    this.currentChannel = channel;
+
+  }
+}
+
+var initTelevision = function initTelevision() {
+  return [...document.querySelectorAll('[data-television]')].map((el => new Television(el)))
+};
+
+
+
+// Init Television
+initTelevision();
